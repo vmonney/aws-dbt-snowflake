6da6be01d07dff2a8f68578e3711740822ded2ed
vmonney <45258650+vmonney@users.noreply.github.com>
2026-03-28 17:17:21 +0100

    Refactor bronze models to use incremental materialization and add new analyses.

--- aws_dbt_snowflake_project/models/bronze/bronze_listings.sql
+++ aws_dbt_snowflake_project/models/bronze/bronze_listings.sql
@@ -1,1 +1,10 @@
+{% set incremental_col = 'CREATED_AT' %}
+
 SELECT * FROM {{ source('staging', 'listings') }}
+
+{% if is_incremental() %}
+    WHERE {{ incremental_col }} > (
+        SELECT COALESCE(MAX({{ incremental_col }}), '1900-01-01'::timestamp_ntz)
+        FROM {{ this }}
+    )
+{% endif %}
